cce6117ffb8c3ec7e6e0812a749860c62c491b0a
Borhan Uddin <borhaninfos@gmail.com>
2022-02-15 20:18:49 +0600

    delete

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Main from "./components/pages/Main";
-
-function App() {
-  return <Main />;
-}
-
-export default App;
--- src/components/pages/header/Header.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import Navigation from "./Navigation";
-
-export default class Header extends Component {
-  render() {
-    return <Navigation />;
-  }
-}
